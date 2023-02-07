4314fefb2f7bf21a3e4f508b45cec858a9861a8d
NasrWejden <wejdnasr98@gmail.com>
2023-02-07 11:40:09 +0100

    Initial Commit

---
+++ MoonShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Wejden Nasr on 7/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Wejden Nasr on 7/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
